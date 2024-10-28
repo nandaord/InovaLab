33937b690228bdcbaff24d25528e43ef70e64757
YgoRosa <ygor18rosa@gmail.com>
2024-10-28 19:30:45 -0300

    Ajeitando os arquivos

---
+++ InovaLab/InovaLab/src/main/java/com/cesar/InovaLab/InovaLab/InovaLabApplication.java
@@ -0,0 +1,13 @@
+package com.cesar.InovaLab.InovaLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InovaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InovaLabApplication.class, args);
+	}
+
+}
